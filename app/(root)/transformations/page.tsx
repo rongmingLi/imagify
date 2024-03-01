c434bd25beb21dcc97322b64f4ca129378707313
屠夫 <fixer1981@163.com>
2024-03-01 10:58:08 +0800

    first commit

---
+++ app/(root)/transformations/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = (params:any) => {
+  return (
+    <div>TransformationsPage {params.id}</div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:3000/transformation/1
