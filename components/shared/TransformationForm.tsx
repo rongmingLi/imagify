1aae2ca3aafb65187218e54b8964393648a596fa
屠夫 <fixer1981@163.com>
2024-03-07 01:26:49 +0800

    image upload toast

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -18,6 +18,7 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { Button } from "../ui/button"
 import { set } from "mongoose"
 import { updateCredits } from "@/lib/actions/user.actions"
+import MediaUploader from "./MediaUploader"
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
@@ -164,6 +165,22 @@ const [isPending,startTransition] = useTransition()
             )}
           </div>
         )}
+        <div className="media-uploader-field">
+          <CustomField 
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+                />
+            )}
+          />
+        </div>
         <div className="flex flex-col gap-4">
         <Button 
           type="button" 
@@ -180,7 +197,7 @@ const [isPending,startTransition] = useTransition()
           </Button>
         </div>
 
-      </form>
+      </form> 
     </Form>
 
   )
